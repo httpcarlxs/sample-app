817b86d2cef3cc1b8b02a0a05a953dab258373e9
Carlos Fuch <httpcarlosx@gmail.com>
2025-05-06 19:10:02 +0000

    feat: time comparison

--- client/client.py
+++ client/client.py
@@ -1,15 +1,17 @@
 import requests
 import time
+from datetime import datetime
 
 def get_hello():
     while True:
         try:
             response = requests.get("http://flask-server:5000/")
-            print(response.text)
+            now = datetime.now().strftime("%H:%M:%S")
+            print(now, '|', response.text)
         except requests.exceptions.RequestException as e:
             print("Error connecting to the server:", e)
 
-        time.sleep(1)
+        time.sleep(5)
 
 if __name__ == "__main__":
     get_hello()
--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    now = datetime.now().strftime("%H:%M:%S")
+    return now + " -- Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
