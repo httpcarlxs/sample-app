47e0e0a85228d2dd6268ad7e1e72c1eece7e4657
httpcarlxs <carlos.fuch@ccc.ufcg.edu.br>
2025-05-27 14:02:52 -0300

    feat: server supports tls communication

--- server/server.py
+++ server/server.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
 from datetime import datetime
+import ssl
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     now = datetime.now().strftime("%H:%M:%S")
-    msg = now + " -- Hello, World!"
+    msg = now + " -- Hello, World com SSL!"
     return jsonify(message=msg)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('tls/server-cert.crt', 'tls/server-key.key')
+    app.run(host="0.0.0.0", port=5000, ssl_context=context)
