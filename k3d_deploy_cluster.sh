ed79179da48f1590528b1f810b035be23bfddb36
sergio-gustavo-andrade-grilo <sergio.gustavo.andrade.grilo@ccc.ufcg.edu.br>
2025-06-13 14:57:19 -0300

    refactor: change server and client to use DockerHub images instead.  Docs: add documentation to files

--- k3d_deploy_cluster.sh
+++ k3d_deploy_cluster.sh
@@ -1,28 +1,14 @@
-k3d registry create sample-registry.localhost --port 5001
-
-k3d cluster create sample-cluster \
-  --registry-use k3d-sample-registry.localhost:5001 \
-  --agents 0 \
-  --servers 1
-
-docker build -f server/Dockerfile -t flask-server:mtls ./server
-docker tag flask-server:mtls k3d-sample-registry.localhost:5001/flask-server:mtls
-docker push k3d-sample-registry.localhost:5001/flask-server:mtls
-
-docker build -f client/Dockerfile -t flask-client:mtls ./client
-docker tag flask-client:mtls k3d-sample-registry.localhost:5001/flask-client:mtls
-docker push k3d-sample-registry.localhost:5001/flask-client:mtls
-
-k3d image import flask-server:mtls --cluster sample-cluster
-k3d image import flask-client:mtls --cluster sample-cluster
+k3d cluster create sample-cluster
 
 kubectl create namespace sample-app
 
+# Configurações para TLS do servidor
 kubectl -n sample-app create secret generic flask-server-tls \
   --from-file=server.crt=./tls/server.crt \
   --from-file=server.key=./tls/server.key \
   --from-file=ca.crt=./tls/ca.crt
 
+# Configurações para TLS do cliente
 kubectl -n sample-app create secret generic flask-client-tls \
   --from-file=client.crt=./tls/client.crt \
   --from-file=client.key=./tls/client.key \
@@ -38,7 +24,9 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring
 
+# ServiceMonitor que serve para monitorar o service do servidor
 kubectl apply -f k8s/flask-servicemonitor.yaml
 
-kubectl create configmap sample-dashboard   --from-file=dashboards/   -n monitoring
-kubectl label configmap sample-dashboard grafana_dashboard=1 -n monitoring
+# ConfigMap que serve para importar automaticamente o dashboard Grafana
+kubectl create configmap sample-dashboard --from-file=dashboards/ --namespace monitoring
+kubectl label configmap sample-dashboard grafana_dashboard=1 --namespace monitoring
--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request
 from datetime import datetime
-import ssl, os
+import ssl
 from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
@@ -13,6 +13,8 @@ def hello_world():
     return jsonify(message=msg)
 
 
+# Prometheus counter metric that counts the amount of requests received by the flask-server. 
+# It is possible to query this metric by inputting "flask_server_requests_total" in the Prometheus query text field.
 server_requests = Counter(
     "flask_server_requests_total",
     "Total amount of requests received by the server",
@@ -21,13 +23,28 @@ server_requests = Counter(
 
 @app.after_request
 def track_metrics(response):
-    if request.path not in ["/favicon.ico", "/metrics"]:
+    """_summary_
+        After each request, update the Prometheus metrics.
+
+    Args:
+        response: The response from the main request made.
+
+    Returns:
+        The response from the main request made.
+    """
+    if request.path != "/metrics":
         server_requests.inc()
     return response
 
 
 @app.route("/metrics")
 def metrics():
+    """_summary_
+        Return the latest Prometheus metrics. The metrics are scraped periodically by Prometheus.
+
+    Returns:
+        The latest Prometheus metrics.
+    """
     return generate_latest(), 200, {"Content-Type": "text/plain"}
 
 
